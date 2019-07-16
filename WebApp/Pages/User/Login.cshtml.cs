d00d2bd17adb0637774428813c8085a19a5e62b9
Adam Tang <ya4dang1@gmail.com>
2019-07-16 23:01:03 +0800

    Implement Mediator for pages

--- WebApp/Pages/User/Login.cshtml.cs
+++ WebApp/Pages/User/Login.cshtml.cs
@@ -1,17 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Modules.User;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages.User
 {
-    public class LoginModel : PageModel
+    public class LoginModel : PageModelBase
     {
+        private readonly IMediator mediator;
+        private readonly IMapper mapper;
+
+        public LoginModel(IMediator mediator)
+        {
+            this.mediator = mediator;
+
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<InputModel, RegisterNewUserCommand>();
+            });
+            mapper = mapperConfig.CreateMapper();
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel : RegisterNewUserCommand
+        {
+            public string Result { get; set; }
+        }
+
         public void OnGet()
         {
+            Input = new InputModel();
+        }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            ///NOTE:For massive fields, use AutoMapper to map the variables
+            var registerNewUserCommand = mapper.Map<RegisterNewUserCommand>(Input);
+            var result = await mediator.Send(registerNewUserCommand);
+
+            if (result.IsError)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                Input.Result = result.Result;
+            }
+
+            return Page();
         }
     }
 }
